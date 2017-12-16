e4faf693dacbb39bfe4c812f8370085648e66b26
Erik Pukinskis <erik@snowedin.net>
2017-12-16 14:13:32 -0800

    BBC Future 18 April 2017 Some experts believe tough decisions to control "the rate at which we deplete natural resources and the rate of pollution" exceed our political and psychological capabilities

--- demo.js
+++ demo.js
@@ -1,82 +1,115 @@
-var tellTheUniverse = require("./")
+var aWildUniverseAppeared = require("a-wild-universe-appeared")
 
 
-// is-everyone-free
-// a-story
-// tell-the-universe
-// someone-is-a-person
-// we-have-people-to-talk-to
 
-tellTheUniverse(
-  "a-story",
-  "is-everyone-free",
-  ["web-element", "browser-bridge", "web-site", "tell-browser", "tell-the-universe"],
-  function aVeryDeepDankSpell() {
+I want to be able to type this shit on my phone:
+
+
+
+aWildUniverseAppeared(
+  "chickens",
+  function() {
+    capture single pixel
+    tap it
+    press to grow square
+    drag to move destination; slow to grow destination segment
+    that's a seed
+    it waves up and down but doesn't move
+
+    add a 2nd dimension to it, right left, and you can attract it to the right attractor or left attractor. if you time it when the seed is above ground, you will be able to stay aloft for multiple pumps of the seed, allowing you to travel longer distances
+
+    drag out another segment (up from the neck), and
+    change direction substantially to add a beak
+    change direction slowly and consistently to add a curve
+    because it is off the ground, it can see, it rotates, and it has its own brain
+
+    tap a segment to train primarily in that brain
+
+    direct the bottom attention left and right to get some air, then direct the top attention to the seed
+
+    when it gets to the seed, the beak eats, and all of the neurons get rewarded. The neurons are just fourier components that have been rewared. Over time a new layer of neurons grows below.
+
+    fourier components are constantly auditioned, and if they match the tapping better than average, they have a slower decay
+
   }
 )
 
-tellTheUniverse(
-  "a-story",
-  "tell-the-universe",
-  ["knox"],
-  function(knox) {
-    // ...
+
+aWildUniverseAppeared(
+  "some-eriks",
+  function eriks(issueBond, sellBond) {
+    var bond = issueBond(
+      "Erik is sane"
+    )
+
+    bond.addTasks([
+      "pay taxes",
+    ])
+
+    bond.addExpense(
+      "rent",
+      "$500"
+    )
+
+    return sellBond(bond)
   }
 )
 
-tellTheUniverse(
-  "a-story",
-  "a-story",
-  ["browser-bridge"],
-  function(bridge, functionCall, makeItEditable) {
+aWildUniverseAppeared(
+  "panel-bond",
+  function freeMoneyBonds(issueBond, sellBond) {
+    var bond = issueBond(
+      "floor panel",
+      {"rateOfReturn": "10%",  "termLength": "60 days"}
+    )
 
+    bond.addTasks([
+      "cut studs to length",
+      "cut track to length",
+      "crimp",
+      "add sheathing",
+      "flipsulate",
+      "add sheathing",
+    ])
 
+    bond.addExpense(
+      "labor",
+      "$100"
+    )
+    bond.addExpense(
+      "steel studs",
+      "$20"
+    )
+    bond.addExpense(
+      "plywood",
+      "$10"
+    )
+
+    return sellBond(bond)
   }
 )
 
-function someoneIsAPerson(number) {
-  var person = new Person(number)
-  person.text("you are a person!")
+aWildUniverseAppeared(
+  "bonds",
+  function capitalismBruh(webHost, showSource) {
 
-  setTimeout(function() {
-    person.text("i am a phone number.")
-  }, 5000)
+    webHost.onVoxel(function(voxel) {
+      voxel("panel-bond")
 
-  person.listen(
-    function(message) {
-      socketToErik.send({
-        smsNeedsResponse: message})
-    }
-  )
-}
+      voxel(
+        function(showSource) {
+          showSource("panel-bond")
+        }
+      )
 
-var socketToErik = new SingleUseSocket()
+      voxel("some-eriks-appeared")
+        // This NPM package pledges to follow the concerns of all Eriks, including avatar services, collective representation, etc. It follows the rules of the Name Package Constitution
 
-var page = [
-  element("someone's phone number"),
-  element("input.persons-number", {placeholder: "number goes here"}),
-  element("button", "they are a person")
-]
+      voxel("name-package-constitution")
 
-bridge.asap(
-  [socketToErik.defineListenOn(bridge)],
-  function(getMessage) {
-    getMessage(function(message) {
-      alert("Someone needs something! "+message)
+      voxel("chickens")
     })
+
   }
 )
 
-webSite.addRoute("get", "/", bridge.sendPage(page))
-
-webSite.addRoute("post", "/people", function(request, response) {
-  someoneIsAPerson(request.body.number)
-  tellTheUniverse(
-    "someoneIsAPerson",
-    request.body.number
-  )
-})
-
-// print f “marigolds wave in the sun, saying hello while wondering where the bees buzz”
-
-site.start(process.env.PORT)
